62e27e1301f4d38593f288647dce29e37cf9855a
Iansiminyu <167209242+Iansiminyu@users.noreply.github.com>
2025-10-20 17:14:38 +0300

    Update phpmailer.lang-sv.php

--- vendor/PHPMailer-6.8.1/language/phpmailer.lang-sv.php
+++ vendor/PHPMailer-6.8.1/language/phpmailer.lang-sv.php
@@ -24,4 +24,4 @@ $PHPMAILER_LANG['signing']              = 'Signeringsfel: ';
 $PHPMAILER_LANG['smtp_connect_failed']  = 'SMTP Connect() misslyckades.';
 $PHPMAILER_LANG['smtp_error']           = 'SMTP serverfel: ';
 $PHPMAILER_LANG['variable_set']         = 'Kunde inte definiera eller återställa variabel: ';
-$PHPMAILER_LANG['extension_missing']    = 'Tillägg ej tillgängligt: ';
+
